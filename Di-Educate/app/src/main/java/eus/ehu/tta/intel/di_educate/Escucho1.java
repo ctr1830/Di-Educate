2028c16552d05a97c572bf8bce62cca7d76103d8
cristian <cristianllagunoiglesias@gmail.com>
2017-01-07 16:54:36 +0100

    Finalizado la funcionalidad escucho mi voz 1

--- Di-Educate/app/src/main/java/eus/ehu/tta/intel/di_educate/Escucho1.java
+++ Di-Educate/app/src/main/java/eus/ehu/tta/intel/di_educate/Escucho1.java
@@ -1,13 +1,87 @@
 package eus.ehu.tta.intel.di_educate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Escucho1 extends AppCompatActivity {
+
+    private static String boton;
+    private static int fail=0;
+    private static String resp_correcta[]={"pato","capucha","coche"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_escucho1);
+
+        Button button1=(Button)this.findViewById(R.id.be11);
+        button1.setText("palo");
+        Button button2=(Button)this.findViewById(R.id.be12);
+        button2.setText("pato");
+    }
+
+    public void respuesta(View v){
+        Button arg0 = (Button) v;
+        boton=arg0.getText().toString();
+        Log.d("boton",boton);
+    }
+    public void audio(View v){
+
+    }
+    public void comprobar (View v){
+        Log.d("correcta",resp_correcta[0]);
+        Button button1=(Button)this.findViewById(R.id.be11);
+        Button button2=(Button)this.findViewById(R.id.be12);
+
+        if(boton.equals(resp_correcta[0])){
+            fail=0;
+            boton=null;
+            //Cambiar audio
+
+            //inicializar botones
+            button1.setText("capucha");
+            button2.setText("babucha");
+        }
+
+        else if(boton.equals(resp_correcta[1])){
+            fail=0;
+            boton=null;
+            //Cambiar audio
+
+            //inicializar botones
+            button1.setText("coche");
+            button2.setText("noche");
+        }
+        else if(boton.equals(resp_correcta[2])){
+            fail=0;
+            boton=null;
+            //Conseguido
+            Intent intent= new Intent(this,CorrectoActivity.class);
+            Bundle extras=new Bundle();
+            extras.putString("opcion","escucho");
+            extras.putString("true","correcto");
+            intent.putExtras(extras);
+            startActivity(intent);
+        }
+        else{
+            fail++;
+
+            //Log.d("AQUI","ENTRE");
+            //Log.d("FAIL",Integer.toString(fail));
+            if(fail==3) {
+                //Añadir audio
+                Log.d("AQUI", "ENTRE tb");
+                Intent intent = new Intent(this, CorrectoActivity.class);
+                Bundle extras = new Bundle();
+                extras.putString("opcion", "escucho");
+                extras.putString("true", "incorrecto");
+                intent.putExtras(extras);
+                startActivity(intent);
+            }
+        }
     }
 }
