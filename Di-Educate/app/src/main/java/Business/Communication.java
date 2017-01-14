90892d3c2e09291dc952fa81c69acfe38b1a3c93
cristian <cristianllagunoiglesias@gmail.com>
2017-01-14 21:40:38 +0100

    Creado el cliente Rest y la clase para las communicaciones con el servidor

---
+++ Di-Educate/app/src/main/java/Business/Communication.java
@@ -0,0 +1,55 @@
+package Business;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+/**
+ * Created by cristian on 14/01/17.
+ */
+
+public abstract class Communication <T> extends AsyncTask<Void, Void, T> {
+    protected final Context context;
+    private final ProgressDialog dialog;
+    private Exception e;
+
+    public Communication (Context context){
+        this.context=context;
+        dialog = new ProgressDialog(context);
+        dialog.setMessage("Conectando ...");
+    }
+
+    @Override
+    protected void onPreExecute(){
+        dialog.show();
+    }
+
+    @Override
+    protected T doInBackground(Void...params){
+        T result=null;
+        try{
+            result=work();
+        }catch(Exception e){
+            this.e=e;
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    protected void onPostExecute(T result){
+        if(dialog.isShowing()){
+            dialog.dismiss();
+        }
+        if(e!=null){
+            Toast.makeText(context,e.getMessage(),Toast.LENGTH_SHORT).show();
+        }
+        else{
+            onFinish(result);
+        }
+    }
+
+    protected abstract T work() throws Exception;
+    protected abstract void onFinish(T result);
+}
