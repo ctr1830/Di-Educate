268bcc926eb74b294c539928a858828ddcb45f9d
cristian <cristianllagunoiglesias@gmail.com>
2017-01-18 18:10:21 +0100

    Finalizado Leo Leo 3

--- Di-Educate/app/src/main/java/eus/ehu/tta/intel/di_educate/LeoLeo3.java
+++ Di-Educate/app/src/main/java/eus/ehu/tta/intel/di_educate/LeoLeo3.java
@@ -11,12 +11,14 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import Business.Communication;
+import Business.ObtenerDatos;
+import Data.Respuestas;
+
 public class LeoLeo3 extends AppCompatActivity {
     private static String boton;
     private static int fail=0;
-    private static String resp_correcta1[]={"lagarto","tomate","tetera"};
-    private static String resp_correcta2[]={"lechuga","gato","topo"};
-    private static String resp_correcta3[]={"cinco","copa","papel"};
+    private ArrayList<String> respuestas=null;
     private static ArrayList<String> array = new ArrayList<String>();
 
     @Override
@@ -24,16 +26,37 @@ public class LeoLeo3 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leo_leo3);
 
+        getRespuestas();
+
         Button button1=(Button)this.findViewById(R.id.bl31);
         button1.setText("tomate");
         Button button2=(Button)this.findViewById(R.id.bl32);
         button2.setText("lagarto");
         Button button3=(Button)this.findViewById(R.id.bl33);
         button3.setText("tetera");
-        TextView textLogin=(TextView)findViewById(R.id.palabra);
-        textLogin.setText(resp_correcta1[0].toUpperCase());
+        //TextView textLogin=(TextView)findViewById(R.id.palabra);
+        //textLogin.setText(respuestas.get(0).toUpperCase());
 
     }
+
+    public void getRespuestas(){
+        new Communication<Respuestas>(this){
+            @Override
+            protected Respuestas work() throws Exception{
+                ObtenerDatos data = new ObtenerDatos();
+                Respuestas respuesta= data.getRespuestas(3);
+                return respuesta;
+            }
+
+            @Override
+            protected void onFinish(Respuestas result) {
+                respuestas=result.getRespuestas();
+                TextView textLogin=(TextView)findViewById(R.id.palabra);
+                textLogin.setText(respuestas.get(0).toUpperCase());
+            }
+        }.execute();
+    }
+
     public void respuesta(View v){
         Button arg0 = (Button) v;
         boton=arg0.getText().toString();
@@ -55,7 +78,7 @@ public class LeoLeo3 extends AppCompatActivity {
             array.add("");
         }
 
-        if((resp_correcta1[0].equals(array.get(0)))&&(resp_correcta1[1].equals(array.get(1)))&&(resp_correcta1[2].equals(array.get(2)))) {
+        if((respuestas.get(0).equals(array.get(0)))&&(respuestas.get(1).equals(array.get(1)))&&(respuestas.get(2).equals(array.get(2)))) {
             fail = 0;
             boton = null;
             array.clear();
@@ -64,9 +87,9 @@ public class LeoLeo3 extends AppCompatActivity {
             button1.setText("gato");
             button2.setText("topo");
             button3.setText("lechuga");
-            textLogin.setText(resp_correcta2[0].toUpperCase());
+            textLogin.setText(respuestas.get(3).toUpperCase());
         }
-        else if((resp_correcta2[0].equals(array.get(0)))&&(resp_correcta2[1].equals(array.get(1)))&&(resp_correcta2[2].equals(array.get(2)))){
+        else if((respuestas.get(3).equals(array.get(0)))&&(respuestas.get(4).equals(array.get(1)))&&(respuestas.get(5).equals(array.get(2)))){
             fail = 0;
             boton = null;
             array.clear();
@@ -74,10 +97,10 @@ public class LeoLeo3 extends AppCompatActivity {
             button1.setText("cinco");
             button2.setText("papel");
             button3.setText("copa");
-            textLogin.setText(resp_correcta3[0].toUpperCase());
+            textLogin.setText(respuestas.get(6).toUpperCase());
         }
 
-        else if((resp_correcta3[0].equals(array.get(0)))&&(resp_correcta3[1].equals(array.get(1)))&&(resp_correcta3[2].equals(array.get(2)))){
+        else if((respuestas.get(6).equals(array.get(0)))&&(respuestas.get(7).equals(array.get(1)))&&(respuestas.get(8).equals(array.get(2)))){
             fail = 0;
             boton = null;
             array.clear();
